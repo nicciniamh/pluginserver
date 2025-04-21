dd07bfb179f576302dbd20a5f159d3583ddd85dc
Nicole Stevens <nicci@ducksfeet.com>
2025-04-21 16:03:21 -0700

    Moved ini file and plugins to /examples

---
+++ examples/plugins/cpuinfo.py
@@ -0,0 +1,25 @@
+from plugincore.baseplugin import BasePlugin
+from aiohttp import web
+import psutil
+
+class CpuInfo(BasePlugin):
+    def request_handler(self, **data):
+        cpudata = {
+            'cpu_count': psutil.cpu_count(),
+            'loadavg': psutil.getloadavg(),
+            'percent': psutil.cpu_percent(),
+            'times': psutil.cpu_times(),
+            'stats': psutil.cpu_stats()
+        }
+        key = data.get('key')
+        if key:
+            if key in cpudata:
+                robj = {key: cpudata[key]}
+            else:
+                return web.json_response({'error': f"key, {key}, not found."}, status=400)
+        else:
+            robj = cpudata
+        
+        return web.json_response(robj, status=200)
+        
+        
