3bb11eea94294c11ccae8f8de2b50d3842a68b62
Nicole Stevens <nicci@ducksfeet.com>
2025-04-21 12:00:11 -0700

    Made baseplugin class more robust, handles auth internally now

--- plugincore/baseplugin.py
+++ plugincore/baseplugin.py
@@ -1,10 +1,39 @@
+from aiohttp import web
 
 class BasePlugin:
+    """
+    This is the base class for plugincore plugins. 
+    The constructor handles setting up the instance variables so the 
+    plugin can play nicely with the plugin manager.
+    """
     def __init__(self, **kwargs):
-        ourclass = self.__class__.__name__
-        self._plugin_id = kwargs.get('route_path',ourclass.lower())
-        #print(f"{self.__class__.__name__}: init - route: '/{self._plugin_id}, args: {kwargs}")
+        self._auth_type = None
+        self._apikey = None
+        self.config = kwargs.get('config')
+        self._plugin_id = kwargs.get('route_path',self.__class__.__name__.lower())
+        self.auth_type = kwargs.get('auth')
+        if self._auth_type:
+            if self._auth_type.lower() == 'global':
+                if 'auth' in self.config and 'apikey' in self.config.auth:
+                    self._apikey = self.config.auth.apikey
+            elif self.auth_type.lower() == 'plugin':
+                self._apikey = kwargs.get('apikey')
+            if not self._apikey:
+                self._auth_type = None
         self.args = dict(kwargs)
+
+    def _check_auth(self,data):
+        if self._auth_type:
+            return self._apikey == data.get('apikey')
+        return True
 
     def _get_plugin_id(self):
         return self._plugin_id
+    
+    async def handle_request(self, **data):
+        if self.auth_type:
+            if not self._check_auth(data):
+                return web.json_response({'error': 'unauthorized'}, status=403)
+
+        return self.request_handler(**data)
+    
