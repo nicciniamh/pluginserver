9d30e4c14779124dc367e7b2bc60d94321623fc3
Nicole Stevens <nicci@ducksfeet.com>
2025-04-22 18:21:32 -0700

    Added dependency check for aiohttp-cors and updated version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='pluginserver',  # still the project name; no need to rename this unless you want
-    version='0.2',
+    version='0.3',
     packages=['plugincore'],  # matches the new directory name
     include_package_data=True,
     install_requires=[
         'aiohttp',
+	'aiohttp_cors',
     ],
     entry_points={
         'console_scripts': [
