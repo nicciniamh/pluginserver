09748f472dc63b2a03afc5481a1955ebb0ae05f7
Nicole Stevens <nicci@ducksfeet.com>
2025-04-30 00:01:36 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pluginserver',
-    version='0.1.3',
+    version='0.1.4',
     packages=['plugincore'],
     include_package_data=True,
     install_requires=[
