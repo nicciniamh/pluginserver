53067a4a214e7afab4684aa3f1abc5fdcb7a189d
Nicole Stevens <nicci@ducksfeet.com>
2025-05-02 21:40:02 -0700

    Moved examples to own repo

--- examples/rsysinfo/venv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# run a program in a default venv - useful for python scripts in systemd
-# edit the path below to point to your venv
-@
-VENV="$1"
-shift
-[[ -d "${VENV}" ]] && source "${VENV}/bin/activate"
-exec $@
