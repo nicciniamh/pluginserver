ea0536e0daf55e68ea5d57c1bf6ed7ca0307e65f
Nicole Stevens <nicci@ducksfeet.com>
2025-05-07 15:37:51 -0700

    Modified fileserver.py to serve markdown as html

--- plugins/fileserve.py
+++ plugins/fileserve.py
@@ -1,7 +1,12 @@
 import os
 import mimetypes
+import re
 from plugincore.baseplugin import BasePlugin
 from aiohttp import web
+try:
+    import markdown
+except ImportError:
+    markdown = None
 
 class ServeFiles(BasePlugin):
     """
@@ -10,8 +15,18 @@ class ServeFiles(BasePlugin):
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.highlight_css = 'highlight.css'
+        self.markdown_css = 'markdown.css'
+        if 'markdown' in self.config:
+            for k in ['highlight','markdown']:
+                css = self.config.markdown.get(f"{k}_css",f"{k}.css") or f"{k}.css"
+                setattr(self,f"{k}_css",css)
+        else:
+            print(f"{type(self)}: WARN: No markdown section in {self.config}")
         self.index_file = self.config.paths.get('indexfile','index.html') or 'index.html'
         print(f"{type(self)}: index_file is {self.index_file}")
+        print(f"{type(self)}: markdown_css is {self.markdown_css}")
+        print(f"{type(self)}: highlight_css is {self.highlight_css}")
         if 'documents' in self.config.paths:
             if ':' in self.config.paths.documents:
                 rpath, dpath = self.config.paths.documents.split(':',1)
@@ -32,6 +47,27 @@ class ServeFiles(BasePlugin):
             </body>
         </html>"""
     
+    def markdown_to_html(self, md):
+        title = ""
+        lines = md.split('\n')
+        for line in lines:
+            match = re.match(r'^\s*#\s+(.*)$',line)
+            if match:
+                title = match.group(1).strip()
+                break
+        mdhtml = markdown.markdown(md, extensions=['fenced_code', 'codehilite'])  # Enable the fenced_code extension
+        return f"""<!DOCTYPE html>
+            <html>
+            <head>
+                <title>{title}</title>
+                <link rel="stylesheet" href="{self.markdown_css}">
+                <link rel="stylesheet" href="{self.highlight_css}">
+            </head>
+            <body>
+                {mdhtml}
+            </body>
+            </html>"""
+    
     async def request_handler(self, **args):
         code = 200
         filename = args.get('subpath',self.index_file) or self.index_file
@@ -44,9 +80,15 @@ class ServeFiles(BasePlugin):
             filename = os.path.join(filename,self.index_file)
         try:
             mime = mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-            print(f"Serving file {filename} with type {mime}")
             with open(filename,'rb') as f:
                 content=f.read()
+            if 'text/markdown' in mime:
+                mime = 'text/html'
+                content = content.decode('utf-8')
+                content = self.markdown_to_html(content)
+                content = content.encode('utf-8')
+
+            print(f"Serving file {filename} with type {mime}")
         except FileNotFoundError as e:
             code, message = 404, 'Resource Not Found'
         except PermissionError as e:
