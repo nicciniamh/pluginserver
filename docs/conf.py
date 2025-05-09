0c3cbabe83be153828e8d10a2e7be5cc019d44c8
Nicole Stevens <nicci@ducksfeet.com>
2025-05-09 10:46:37 -0700

    New theme?

--- docs/conf.py
+++ docs/conf.py
@@ -33,3 +33,50 @@ source_suffix = {
 }
 master_doc = 'index'
 
+
+# Material theme options (minimal dark mode)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    'nav_title': 'Plugin Server',
+
+    # Set the color and the accent color
+    'color_primary': 'grey',
+    'color_accent': 'deep-orange',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/nicciniamh/pluginserver',
+    'repo_name': 'Plugin Server',
+
+    # Configure the palette to use dark mode
+    'theme_color': 'black',  # for meta theme-color
+    'globaltoc_depth': 2,
+    'globaltoc_collapse': True,
+    'globaltoc_includehidden': True,
+
+    'master_doc': False,  # suppress 'Contents' heading
+
+    # Dark mode palette config
+    'palette': [
+        {
+            'media': '(prefers-color-scheme: dark)',
+            'scheme': 'slate',
+            'primary': 'grey',
+            'accent': 'deep-orange',
+            'toggle': {
+                'icon': 'material/weather-sunny',
+                'name': 'Switch to light mode',
+            },
+        },
+        {
+            'media': '(prefers-color-scheme: light)',
+            'scheme': 'default',
+            'primary': 'grey',
+            'accent': 'deep-orange',
+            'toggle': {
+                'icon': 'material/weather-night',
+                'name': 'Switch to dark mode',
+            },
+        },
+    ],
+}
+
