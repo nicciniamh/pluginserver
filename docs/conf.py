d2bbb64eeb01196cbe2cd0220b4c6e8d7d32020f
Nicole Stevens <nicci@ducksfeet.com>
2025-05-09 16:13:38 -0700

    trying themes

--- docs/conf.py
+++ docs/conf.py
@@ -5,18 +5,21 @@ author = 'Nicole Stevens'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'myst_parser',  # Markdown support
+    'myst_parser',     # Markdown support
+    'sphinx_github_style', # Enable the github-style theme/extension
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme' # You can try changing this to a theme provided by sphinx-github-style if it has one
 
 source_suffix = {
     '.rst': 'restructuredtext',
-    '.md': 'markdown',  # Markdown files with myst-parser
+    '.md': 'markdown',    # Markdown files with myst-parser
 }
 master_doc = 'index'
 
+# -- Configuration for sphinx-github-style ------------------------------------
+linkcode_url = "https://github.com/nicciniamh/pluginserver"
