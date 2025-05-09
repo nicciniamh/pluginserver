ae8b85bc8ff63c2795a88d99f39547cdcec893ce
Nicole Stevens <nicci@ducksfeet.com>
2025-05-09 10:50:48 -0700

    New theme?

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #html_theme = 'sphinx_rtd_theme'
-html_theme = "sphinx_material"
+html_theme = "furo"
 html_static_path = ['_static']
 
 source_suffix = {
@@ -32,51 +32,3 @@ source_suffix = {
     '.md': 'markdown',
 }
 master_doc = 'index'
-
-
-# Material theme options (minimal dark mode)
-html_theme_options = {
-    # Set the name of the project to appear in the sidebar
-    'nav_title': 'Plugin Server',
-
-    # Set the color and the accent color
-    'color_primary': 'grey',
-    'color_accent': 'deep-orange',
-
-    # Set the repo location to get a badge with stats
-    'repo_url': 'https://github.com/nicciniamh/pluginserver',
-    'repo_name': 'Plugin Server',
-
-    # Configure the palette to use dark mode
-    'theme_color': 'black',  # for meta theme-color
-    'globaltoc_depth': 2,
-    'globaltoc_collapse': True,
-    'globaltoc_includehidden': True,
-
-    'master_doc': False,  # suppress 'Contents' heading
-
-    # Dark mode palette config
-    'palette': [
-        {
-            'media': '(prefers-color-scheme: dark)',
-            'scheme': 'slate',
-            'primary': 'grey',
-            'accent': 'deep-orange',
-            'toggle': {
-                'icon': 'material/weather-sunny',
-                'name': 'Switch to light mode',
-            },
-        },
-        {
-            'media': '(prefers-color-scheme: light)',
-            'scheme': 'default',
-            'primary': 'grey',
-            'accent': 'deep-orange',
-            'toggle': {
-                'icon': 'material/weather-night',
-                'name': 'Switch to dark mode',
-            },
-        },
-    ],
-}
-
