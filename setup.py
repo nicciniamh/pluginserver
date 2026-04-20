4d57049153c028cb18dc66af8f41dcbfdbd717a5
Nicole <nicci@ducksfeet.com>
2026-04-20 16:24:33 -0700

    updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='pluginserver',
-    version='0.10.2',
+    version='0.10.3',
     packages=['plugincore'],
     include_package_data=True,
     description='Plugin-driven API server',
